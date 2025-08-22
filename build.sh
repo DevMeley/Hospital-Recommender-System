c02396a5570a5d6af8b52b0b6d2cdb11ac540685
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-08-22 21:40:31 +0100

    fix: Update copy command in build script to reference correct directory for React build

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ npm run build
 # Copy React build into FastAPI's static folder
 rm -rf ../hospital-recommender-server/static
 mkdir -p ../hospital-recommender-server/static
-cp -r build/* ../hospital-recommender-server/static/
+cp -r dist/* ../hospital-recommender-server/static/
 
 # Go back to backend
 cd ../hospital-recommender-server
